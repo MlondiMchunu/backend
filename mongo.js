9ddbd1409de6f92ae3d46136532c45d7f760913f
Stopa <mlondiemchunu1@gmail.com>
2024-09-14 13:07:40 +0200

    printing stored notes

--- mongo.js
+++ mongo.js
@@ -20,18 +20,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note',noteSchema)
 
-const note = new Note({
+/*const note = new Note({
     content: 'This is a fourth note',
     important: true,
 })
-
-/*Note.find({}).then(result =>{
+*/
+Note.find({}).then(result =>{
     result.forEach(note =>{
         console.log(note)
     })
-})*/
+})
 
-note.save().then(result=>{
+/*note.save().then(result=>{
     console.log('note saved!')
     mongoose.connection.close()
 })
+*/
