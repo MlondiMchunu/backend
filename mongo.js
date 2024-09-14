ff72f77a533cb3a2f2832c43f7de748d1e934d5a
Stopa <mlondiemchunu1@gmail.com>
2024-09-14 10:47:23 +0200

    mongo client url

--- mongo.js
+++ mongo.js
@@ -2,4 +2,9 @@ const mongoose = require('mongoose')
 
 if (process.argv.length < 3){
     console.log('give password as argument')
+    process.exit(1)
 }
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://mlondiemchunu1:Maxingwane12@cluster0.oveo9.mongodb.net/`
