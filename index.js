38ac20171c287f8e983b9fbc3a5b7970ae7f0ec4
Stopa <mlondiemchunu1@gmail.com>
2024-10-10 15:59:00 +0200

    error handler complete

--- index.js
+++ index.js
@@ -119,11 +119,15 @@ app.get('/api/notes/:id',(req,res,next)=>{
         .catch(error=>next(error))
 })
 
-const errorHandler = (error, request, response, next)=>{
+const errorHandler = (error, req, res, next)=>{
     console.error(error.message)
 
-    
+    if(error.name === 'CastError'){
+        return res.status(400).send({error:'malformatted id'})
+    }
+    next(error)
 }
+app.use(errorHandler)
 
 
 //another postman code
