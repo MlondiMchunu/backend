4c6a7f969fa66e2bdb608fe9af9c2194eb155223
Stopa <mlondiemchunu1@gmail.com>
2024-10-10 11:15:23 +0200

    environment setup

---
+++ models/note.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery',false)
+
+require('dotenv').config();
+const url = process.env.MONGODB_URI;
+
+console.log('connectiong to',url)
+
+mongoose.connect(url)
+    .then(result=>{
+        console.log('connected to mongoDB');
+    })
+    .catch(error=>{
+        console.log('error connectiong to mongodb')
+    })
+
