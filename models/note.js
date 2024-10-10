47497dea3291152ff6e6c13197d1c63ad59847c7
Stopa <mlondiemchunu1@gmail.com>
2024-10-10 11:17:54 +0200

    noteSchema creation

--- models/note.js
+++ models/note.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 
 require('dotenv').config();
 const url = process.env.MONGODB_URI;
 
-console.log('connectiong to',url)
+console.log('connectiong to', url)
 
 mongoose.connect(url)
-    .then(result=>{
+    .then(result => {
         console.log('connected to mongoDB');
     })
-    .catch(error=>{
+    .catch(error => {
         console.log('error connectiong to mongodb')
     })
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
 
