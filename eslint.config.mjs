138207ffe9b4d74f4d18175dfcffccf7dabda7e5
Stopa <mlondiemchunu1@gmail.com>
2024-10-11 16:44:07 +0200

    latest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,11 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import pluginJs from "@eslint/js"
+import js from "@eslint/js"
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 
 export default [
+  js.configs.recommend,
   { 
     files: ["**/*.js"], 
     languageOptions: { 
@@ -15,6 +18,29 @@ export default [
         ...globals.browser, ...globals.node 
       } 
     } 
+  },
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'never'
+      ],
+    },
   },
   pluginJs.configs.recommended,
 ];
