67eb413971bb2c6f5dec6882dbcf759ae07830a5
Stopa <mlondiemchunu1@gmail.com>
2024-10-12 16:16:02 +0200

    average function of test

--- utils/for_testing.js
+++ utils/for_testing.js
@@ -4,3 +4,10 @@ const reverse = (string)=>{
         .reverse()
         .join('')
 }
+
+const average = (array)=>{
+    const reducer = (sum, item)=>{
+        return sum + item
+    }
+    return array.reduce(reducer, 0)/array.length
+}
