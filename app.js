5e892ba5e2664dc85c3b10f52cd37849b904541f
Stopa <mlondiemchunu1@gmail.com>
2024-10-12 11:19:39 +0200

    commit route handlers2 and export app module

--- app.js
+++ app.js
@@ -23,4 +23,10 @@ app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
+
 app.use('/api/notes', notesRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
