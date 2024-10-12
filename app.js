db225303a265db68e2d664db72b8754ee868034b
Stopa <mlondiemchunu1@gmail.com>
2024-10-12 11:15:13 +0200

    latest changes

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
 
-app.use('/api/notes',notesRouter)
+app.use('/api/notes', notesRouter)
