99f42c2f7eddbc2be7b21e11dbca5d76d041de64
Stopa <mlondiemchunu1@gmail.com>
2024-10-12 17:51:42 +0200

    commit test1 file

---
+++ tests/reverse.test.js
@@ -0,0 +1,10 @@
+const {test} = require('node:test')
+const assert = require('node:assert')
+
+const reverse = require('../utils/for_testing').reverse
+
+test('reverse of a', ()=>{
+    const result = reverse('a')
+
+    assert.strictEqual(result, 'a')
+})
