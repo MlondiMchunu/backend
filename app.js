5c9a01793d94164a81da75c0223e241f47d07e65
Stopa <mlondiemchunu1@gmail.com>
2024-10-12 11:18:06 +0200

    commit route handlers

--- app.js
+++ app.js
@@ -19,4 +19,8 @@ mongoose.connect(config.MONGODB_URI)
         logger.error('error connecting to MongoDB:', error.message)
     })
 
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
 app.use('/api/notes', notesRouter)
