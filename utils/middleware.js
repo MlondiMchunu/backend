11a01e4f748eb6fb672ce38833acb524e54f3d23
Stopa <mlondiemchunu1@gmail.com>
2024-10-12 11:32:05 +0200

    commit errorHandler

--- utils/middleware.js
+++ utils/middleware.js
@@ -11,3 +11,15 @@ const requestLogger = (req,res,next)=>{
 const unknownEndpoint = (req, res)=>{
     res.status(404).send({error: 'unknown endpoint'})
 }
+
+const errorHandler = (error,req,res,next)=>{
+    logger.error(error.message)
+
+    if (error.name === 'CastError'){
+        return res.status(400).send({error: 'malformatted id'})
+    }else if(error.name === 'ValidationError'){
+        return res.status(400).json({error: error.message})
+    }
+
+    next(error)
+}
