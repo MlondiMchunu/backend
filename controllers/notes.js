6e625a7eef270f2cdbffde25c8455f627c7504ce
Stopa <mlondiemchunu1@gmail.com>
2024-10-14 14:45:31 +0200

    add error handling (try-catch)

--- controllers/notes.js
+++ controllers/notes.js
@@ -11,53 +11,57 @@ const Note = require('../models/note')
     */
 
 //promise 
-notesRouter.get('/',async(req,res)=>{
+notesRouter.get('/', async (req, res) => {
     const notes = await Note.find({})
     res.json(notes)
 })
 
-notesRouter.get('/:id',(req,res,next)=>{
+notesRouter.get('/:id', (req, res, next) => {
     Note.findById(req.params.id)
-        .then(note =>{
-            if(note){
+        .then(note => {
+            if (note) {
                 res.json(note)
-            }else{
+            } else {
                 res.status(404).end()
             }
         })
         .catch(error => next(error))
 })
 
-notesRouter.post('/', async(req,res,next)=>{
+notesRouter.post('/', async (req, res, next) => {
     const body = req.body
 
     const note = new Note({
         content: body.content,
         important: body.important || false,
     })
+    //use try catch for error handling in async await
+    try {
+        const savedNote = await note.save()
+        res.status(201).json(savedNote)
+    } catch (exception) {
+        next(exception)
+    }
 
-    const savedNote = await note.save()
-    res.status(201).json(savedNote)
-  
 })
 
-notesRouter.delete('/:id',(req,res,next)=>{
+notesRouter.delete('/:id', (req, res, next) => {
     Note.findByIdAndDelete(req.params.id)
-        .then(()=>{
+        .then(() => {
             res.status(204).end()
         })
         .catch(error => next(error))
 })
 
-notesRouter.put('/:id',(req,res,next)=>{
+notesRouter.put('/:id', (req, res, next) => {
     const body = req.body
 
     const note = {
         content: body.content,
         important: body.important,
     }
-    Note.findByIdAndUpdate(req.params.id, note,{new: true})
-        .then(updatedNote=>{
+    Note.findByIdAndUpdate(req.params.id, note, { new: true })
+        .then(updatedNote => {
             res.json(updatedNote)
         })
         .catch(error => next(error))
