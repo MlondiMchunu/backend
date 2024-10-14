0561f019a78f48a13ed69c4f2e5bbcbda76f8186
Stopa <mlondiemchunu1@gmail.com>
2024-10-14 09:58:29 +0200

    commit first test file

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -4,3 +4,14 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
+
+test('notes are returned as json', async()=>{
+    await api 
+        .get('/api/notes')
+        .expect(200)
+        .expect('Content-Type','/application\/json')
+})
+
+after(async()=>{
+    await mongoose.connection.close()
+})
