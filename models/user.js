89874d7daaa52c9a014563572744eb7604decf31
MlondiMchunu <mlondiemchunu1@address.com>
2024-10-22 13:09:08 +0200

    format returned data

--- models/user.js
+++ models/user.js
@@ -9,5 +9,16 @@ const userSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Note'
         }
-    ]
+    ],
+})
+
+userSchema.set('toJSON',{
+    transform: (document, returnedObject)=>{
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        //the passwordHash should not be revealed
+        delete returnedObject.passwordHash
+
+    }
 })
