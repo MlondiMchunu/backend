26cc9c78d336fcc93a4a2d84618b073da26cf79d
MlondiMchunu <mlondiemchunu1@address.com>
2024-10-22 13:31:59 +0200

    create user-router

--- controllers/users.js
+++ controllers/users.js
@@ -1,2 +1,8 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
+const User = require('./models/user')
+
+usersRouter.post('/',async(req,res)=>{
+    const {username, name, password} = req.body
+    
+})
