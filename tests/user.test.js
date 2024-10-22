115aa955c060351e831497788fb6c914c5e60e0c
mlondiemchunu1@gmail.com <mlondiemchunu1@address.com>
2024-10-22 13:50:16 +0200

    save user

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,2 +1,13 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
+
+describe('when there is initially one user in db',()=>{
+    beforeEach(async()=>{
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret',10)
+        const user = new User({username:'root',passwordHash})
+
+        await user.save
+    })
+})
