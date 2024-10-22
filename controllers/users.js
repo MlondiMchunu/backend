d3aab897ef80321c53b3605c153330cc8e0a06b8
MlondiMchunu <mlondiemchunu1@address.com>
2024-10-22 13:33:15 +0200

    hashPassword

--- controllers/users.js
+++ controllers/users.js
@@ -4,5 +4,7 @@ const User = require('./models/user')
 
 usersRouter.post('/',async(req,res)=>{
     const {username, name, password} = req.body
-    
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
 })
