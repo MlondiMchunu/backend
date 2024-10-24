2fabbcd6254b09f8ce689f91a3fe842e12f9aab9
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-10-24 21:39:41 +0200

    commit userForToken function

--- controllers/login.js
+++ controllers/login.js
@@ -6,15 +6,19 @@ const User = require('../models/user')
 loginRouter.post('/', async (req, res) => {
     const { username, password } = req.body
 
-    const user = await User.findOne({username})
+    const user = await User.findOne({ username })
     const passwordCorrect = user === null
         ? false
         : await bcrypt.compare(password, user.passwordHash)
 
-        if(!(user && passwordCorrect)){
-            return res.status(401).json({
-                error: 'invalid username or password'
-            })
-        }
+    if (!(user && passwordCorrect)) {
+        return res.status(401).json({
+            error: 'invalid username or password'
+        })
+    }
 
+    const userForToken = {
+        username: user.username,
+        id: user._id,
+    }
 })
