f843279c8d08d3aa4b40a6e6a4374a9b2fbb0566
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-10-24 21:14:12 +0200

    login req

--- controllers/login.js
+++ controllers/login.js
@@ -2,3 +2,9 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
+
+loginRouter.post('/', async (req, res) => {
+    const { username, password } = req.body
+
+    
+})
