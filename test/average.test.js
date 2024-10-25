e3695bc95b24363f52d764705a4377e87e52f3ef
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-10-25 16:17:53 +0200

    average function tests debug

--- test/average.test.js
+++ test/average.test.js
@@ -1,6 +1,6 @@
 const average = require('../utils/for_testing').average
 
-describe('average', () => {
+//describe('average', () => {
   test('of one value is the value itself', () => {
     expect(average([1])).toBe(1)
   })
@@ -12,4 +12,4 @@ describe('average', () => {
   test('of empty array is zero', () => {
     expect(average([])).toBe(0)
   })
-})
+//})
