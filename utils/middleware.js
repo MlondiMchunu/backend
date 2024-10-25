0d56e8ff1c2b41deebc33aad29b9eb8128ec749d
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-10-25 12:19:30 +0200

    fix variable in error handler

--- utils/middleware.js
+++ utils/middleware.js
@@ -21,7 +21,7 @@ const errorHandler = (error,req,res,next)=>{
         return res.status(400).json({error: error.message})
     }else if(error.name === 'MongoServerError' && 
         error.message.includes('E11000 duplicate key error')){
-            return response.status(400).json({error:'expected `username` to be unique'})
+            return res.status(400).json({error:'expected `username` to be unique'})
         }
 
     next(error)
